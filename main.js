e6344105ad6607c93669314be7e058848726fe30
Amrutha S <amrutha.s.2710@gmail.com>
2025-07-19 18:42:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-const fs=require("fs")
-const fs=require("fs/mainpromise")
+const express = require('express')
+const app = express()
+const port = 4000
+const blog = require('./routes/blog')
+const fs = require("fs")
 
-console.log("starting")
+// app.use(express.static("public"))
 
-fs.writeFile("ammu.txt","good best",()=>{
-    console.log("file created")
+app.use('/blog', blog)
+
+// Middleware 1 - Logger for our application
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.harry = "I am harry bhai";
+    fs.appendFileSync("logs.txt", `${Date.now()} is a ${req.method}\n`)
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send("Hacked by Middlware 1")
+    next()
 })
-fs.readFile("ammu.txt","utf-8",(err,data)=>{
-    console.log(err,data.toString())
+
+// Middleware 2
+app.use((req, res, next) => {
+    console.log('m2')
+    req.harry = "I am Rohan bhai";
+    next()
 })
 
-fs.appendFile("ammu.txt"," and better",(e,d)=>{
-    console.log("file updated")
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+    res.send('Hello about!' + req.harry)
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
